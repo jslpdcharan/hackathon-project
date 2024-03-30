8e79a8cc1a1c8e791bcb83beede2f3e12bcda33e
Hdwarak <165440914+Hdwarak@users.noreply.github.com>
2024-03-30 17:00:13 -0500

    Add Alumni Page

---
+++ src/pages/Alumni.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ImagePath from '../images/osu_alumni_center_banner.jpg';
+import IconPath from '../images/linkedin.jpeg'; // Replace with the actual path to your icon
+
+const AlumniPage = () => {
+    const [alumniByYear, setAlumniByYear] = useState({});
+
+    useEffect(() => {
+        const fetchAlumni = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/api/alumni_members');
+                const alumni = response.data; // Assuming the API returns an array of alumni
+                const alumniGroupedByYear = alumni.reduce((acc, curr) => {
+                    const year = curr.a_year_of_enrollment;
+                    if (!acc[year]) {
+                        acc[year] = [];
+                    }
+                    acc[year].push(curr);
+                    return acc;
+                }, {});
+                setAlumniByYear(alumniGroupedByYear);
+            } catch (error) {
+                console.error("Error fetching alumni:", error);
+            }
+        };
+        fetchAlumni();
+    }, []);
+
+    return (
+        <div style={{
+            backgroundImage: `url(${ImagePath})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat',
+            minHeight: '100vh',
+        }}>
+            <div style={{
+                backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                minHeight: '100vh',
+            }}>
+                <div className="container mt-4">
+                    <h2 className="mb-4" style={{ color: 'rgb(228 93 17)' }}>Alumni Members by Year</h2>
+                    {Object.keys(alumniByYear).sort().map(year => (
+                        <div key={year}>
+                            <h3 className="my-3">{year}</h3>
+                            <div className="row">
+                                {alumniByYear[year].map((alumnus, index) => (
+                                    <div key={index} className="col-lg-3 col-md-6 mb-4">
+                                        <div className="card h-100" onClick={() => window.open('https://www.linkedin.com/school/oklahoma-state-university/', '_blank')}>
+                                            <div className="card-body" style={{ cursor: 'pointer' }}>
+                                                <h5 className="card-title">
+                                                    {alumnus.a_full_name}
+                                                    <img
+                                                        src={IconPath} // Your icon image path
+                                                        alt="Icon"
+                                                        style={{ width: '20px', height: '20px', marginLeft:'10px' }} // Adjust size as needed
+                                                    />
+
+                                                </h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AlumniPage;
