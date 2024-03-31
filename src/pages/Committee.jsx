ffc59dbac297305811f60974d0b29b7ff16a11dc
Hdwarak <165440914+Hdwarak@users.noreply.github.com>
2024-03-31 11:54:29 -0500

    Update Committee.jsx (#20)

--- src/pages/Committee.jsx
+++ src/pages/Committee.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import image from '../images/pete_face.png';
 
 const CommitteeMembers = () => {
     const [committeeMembers, setCommitteeMembers] = useState([]);
@@ -32,23 +33,33 @@ const CommitteeMembers = () => {
         fetchData();
         committeeMembers.forEach((member) => fetchResponsibilities(member.m_role));
     }, [committeeMembers]);
-
     return (
         <div className="container">
             {committeeMembers.map((member) => (
-                <div key={member.m_name} className="row mb-4 border rounded p-3">
-                    <div className="col-md-6 " style={{padding: "20px"}}>
-                        <br></br>
-                        <br></br>
-
-                        <h4>{member.m_role}</h4>
-                        <p>Name: {member.m_name}</p>
-                        <p>Email: {member.m_email}</p>
-                        <p>Role: {member.m_role}</p>
-                        <p>Level: {member.m_level}</p>
+                <div
+                    key={member.m_name}
+                    className="row mb-4 p-3 align-items-center"
+                    style={{
+                        borderColor: 'orange', // Adding an orange border
+                        borderWidth: '1px',
+                        borderStyle: 'solid',
+                        borderRadius: '0.25rem', // Rounded corners
+                        backgroundColor: '#fff', // Assuming a white background
+                        boxShadow: '0 2px 4px rgba(0,0,0,.1)' // A subtle shadow for depth
+                    }}
+                >
+                    <div className="col-lg-2 col-md-3 col-sm-4 col-12 d-flex align-items-center justify-content-center p-2">
+                        <img src={image} alt={member.m_name} className="img-fluid" style={{ maxWidth: '100px' }}/>
                     </div>
-                    <div className="col-md-6 text-start">
-                        <h4>{member.m_role} Responsibilities</h4>
+                    <div className="col-lg-4 col-md-5 col-sm-8 col-12">
+                        <h4 style={{ color: 'orange' }}>{member.m_role}</h4> {/* Role in orange */}
+                        <p><strong>Name:</strong> {member.m_name}</p>
+                        <p><strong>Email:</strong> {member.m_email}</p>
+                        <p><strong>Role:</strong> {member.m_role}</p>
+                        <p><strong>Level:</strong> {member.m_level}</p>
+                    </div>
+                    <div className="col-lg-6 col-md-4 col-sm-12 col-12 text-start">
+                        <h4 style={{ color: 'orange' }}>Responsibilities</h4> {/* Responsibilities header in orange */}
                         {memberResponsibilities[member.m_role] && (
                             <ul>
                                 {memberResponsibilities[member.m_role].map((responsibility, index) => (
